bacd22923c75ca8efa81803b3d434bac7549ae3d
Naresh <naresh@Nareshs-MacBook-Pro.local>
2017-12-13 00:34:25 +0530

    Initial Commit

---
+++ new/new.h
@@ -0,0 +1,19 @@
+//
+//  new.h
+//  new
+//
+//  Created by Naresh on 13/12/17.
+//  Copyright © 2017 Naresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for new.
+FOUNDATION_EXPORT double newVersionNumber;
+
+//! Project version string for new.
+FOUNDATION_EXPORT const unsigned char newVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <new/PublicHeader.h>
+
+
