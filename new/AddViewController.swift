35da7c3a56ab3c2ee0e01b30a5828cc0de7d3d5e
Naresh <naresh@Nareshs-MacBook-Pro.local>
2017-12-13 00:35:48 +0530

    initial

---
+++ new/AddViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddViewController.swift
+//  new
+//
+//  Created by Naresh on 13/12/17.
+//  Copyright © 2017 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+    func add(a:Int,b:Int) -> Int {
+        return a+b
+    }
+
+}
